6a9444c32f3cef4c9c661f3c335c4862dd35db1e
Abhinav Singh <120677137+abhinav-dops@users.noreply.github.com>
2025-08-07 23:59:06 +0530

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -9,7 +9,7 @@ if os.path.exists("expenses.json"):
 else:
     expenses = []
 
-
+kaj
 def show_total_expenses():    #  show total spending.
     with open("expenses.json", "r") as file:
         expenses = json.load(file)
